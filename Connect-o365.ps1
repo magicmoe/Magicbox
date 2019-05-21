69c48f27fa03ac38a69da26dc02fa4a4f34db14a
magicmoe <50865947+magicmoe@users.noreply.github.com>
2019-05-21 13:20:12 +0100

    added remove comment

--- Connect-o365.ps1
+++ Connect-o365.ps1
@@ -3,4 +3,6 @@ $UserName = Read-Host "UserName"
 $Password = Read-Host "Password" -AsSecureString
 $Cred = New-Object System.Management.Automation.PSCredential($UserName,$Password)
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Cred -Authentication Basic -AllowRedirection 
-Import-PSSession $Session 
+Import-PSSession $Session
+
+#Remove-PSSession $Session
