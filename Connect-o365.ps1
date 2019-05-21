f5d9aa3fa26cf79c638d08260b42591cfdac58bc
Mohamed Ahmed <mohammed.ahmed079@gmail.com>
2019-05-21 13:17:15 +0100

    put a comment on script

--- Connect-o365.ps1
+++ Connect-o365.ps1
@@ -1,4 +1,5 @@
-﻿$UserName = Read-Host "UserName"
+﻿#Use this script to connect to exchange online
+$UserName = Read-Host "UserName"
 $Password = Read-Host "Password" -AsSecureString
 $Cred = New-Object System.Management.Automation.PSCredential($UserName,$Password)
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Cred -Authentication Basic -AllowRedirection 
