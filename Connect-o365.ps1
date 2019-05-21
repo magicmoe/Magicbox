83f7b2d89e249b4d9f890c2dd83cddff2d89bca8
Mohamed Ahmed <MAhmed@britishmuseum.org>
2019-05-21 13:09:04 +0100

    added o365 psfile

---
+++ Connect-o365.ps1
@@ -0,0 +1,5 @@
+﻿$UserName = Read-Host "UserName"
+$Password = Read-Host "Password" -AsSecureString
+$Cred = New-Object System.Management.Automation.PSCredential($UserName,$Password)
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Cred -Authentication Basic -AllowRedirection 
+Import-PSSession $Session 
